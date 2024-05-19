5814f9a1874edc105975eff2bc7dbb930aae7b45
jessie-sun927 <73707760+jessie-sun927@users.noreply.github.com>
2024-05-19 16:37:56 -0400

    Update main.py and delete animated_st.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error
@@ -13,6 +14,7 @@ header = st.container()
 dataset = st.container()
 features = st.container()
 model_training = st.container()
+plotly_demo = st.container()
 
 # Header section
 with header:
@@ -35,8 +37,8 @@ with dataset:
 with features:
     st.header("Features Section")
     st.text("This section can be used to discuss features of the project.")
-    st.markdown('* **first feature: ** I created this feature because of this ...')
-    st.markdown('* **second feature: ** I created this feature because of this ...')
+    st.markdown('* **first feature:**')
+    st.markdown('* **second feature:**')
 
 # model training section
 with model_training:
@@ -67,6 +69,56 @@ with model_training:
 
     disp_col.subheader("Mean absolute error of model is")
     disp_col.write(mean_absolute_error(y, prediction))
+
+
+with plotly_demo:
+    st.header("Plotly Demo")
+    st.text("This section demostrates the creation of animated plots with Plotly")
+    df = px.data.gapminder()
+
+    st.write(df)
+
+    year_options=df['year'].unique().tolist()
+    year = st.selectbox('Which year would you like to see?', year_options, 0)
+    # df = df[df['year'] == year] # commented out for animation
+
+    fig = px.scatter(df, x = 'gdpPercap', y = 'lifeExp',
+        size = 'pop', color = 'continent', hover_name = 'continent', 
+        log_x = True, size_max = 55, range_x = [100, 100000], range_y = [25, 90],
+        animation_frame = 'year', animation_group = 'country')
+
+    fig.update_layout(width = 800)
+    st.write(fig)
+
+
+
+    covid = pd.read_csv('https://raw.githubusercontent.com/shinokada/covid-19-stats/master/data/daily-new-confirmed-cases-of-covid-19-tests-per-case.csv')
+    covid.columns = ['Country', 'Code', 'Date', 'Confirmed', 'Days since confirmed']
+    covid['Date'] = pd.to_datetime(covid['Date']).dt.strftime('%Y-%m-%d')
+    country_options = covid['Country'].unique().tolist()
+
+    st.write(covid)
+
+    date_options = covid['Date'].unique().tolist()
+    date = st.selectbox('Which date would you like to see?', date_options, 100)
+    country = st.multiselect('Which country would you like to see?', country_options, ['Brazil'])
+
+
+    covid = covid[covid['Country'].isin(country)]
+    # covid = covid[covid['Date'] == date] 
+
+    fig2 = px.bar(covid, x = 'Country', y = 'Confirmed', color = 'Country',
+        range_y = [0, 35000], animation_frame = 'Date', animation_group = 'Country')
+
+    # set animation speed
+    fig2.layout.updatemenus[0].buttons[0].args[1]['frame']['duration'] = 30
+    fig2.layout.updatemenus[0].buttons[0].args[1]['transition']['duration'] = 5
+
+    fig2.update_layout(width = 800)
+    st.write(fig2)
+
+
+
 
 
 
